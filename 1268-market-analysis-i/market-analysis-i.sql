802341875c642498d8a8a19c89a0e03dcf2f7ecf
Smriti Dubey <smritidubey14nov@gmail.com>
2026-01-07 13:58:27 +0530

    Time: 1267 ms (26.79%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select 
-u.user_id as buyer_id, u.join_date, count(o.order_id) as orders_in_2019
+u.user_id as buyer_id, u.join_date, IFNULL(count(o.order_id),0) as orders_in_2019
 from Users u
 left join Orders o 
 on u.user_id = o.buyer_id and YEAR(order_date) = '2019'
