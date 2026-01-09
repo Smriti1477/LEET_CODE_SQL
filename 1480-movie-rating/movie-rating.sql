7ea02e0bcff905226c3e744b59107be381deda53
Smriti Dubey <smritidubey14nov@gmail.com>
2026-01-09 23:51:28 +0530

    Time: 1364 ms (22.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,33 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+with highest_user_rated as(
+    select u.name from Movierating m
+    left join users u
+    on m.user_id = u.user_id
+    group by m.user_id,u.name
+    order by count(m.user_id) desc, u.name asc
+    limit 1
+),
+
+highest_avg_rated as(
+    select m1.title from movies m1
+    right join movierating m
+    on m1.movie_id = m.movie_id
+    where m.created_at >='2020-02-01' and m.created_at < '2020-03-01'
+    group by m1.title
+    order by avg(m.rating) desc,title asc
+    limit 1
+),
+
+output as(
+    select * from highest_user_rated
+    union all
+    select * from highest_avg_rated
+)
+select name as results from output
+
+
+
+
+    
