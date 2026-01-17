a492717592e698609ba538a9db38ebb277fd3dfc
Smriti Dubey <smritidubey14nov@gmail.com>
2026-01-18 00:07:14 +0530

    Time: 313 ms (47.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 4025-find-loyal-customers/find-loyal-customers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select customer_id
+from customer_transactions
+group by customer_id
+having sum(case when transaction_type = 'purchase' then 1 else 0 end)>= 3
+and DATEDIFF(max(transaction_date), min(transaction_date))>= 30
+and sum(case when transaction_type ='refund' then 1 else 0 end)*1.0/count(*) < 0.2
+order by customer_id
