1665cc8d2b535687c96667f3e537060c79987c52
Smriti Dubey <smritidubey14nov@gmail.com>
2026-01-19 22:39:57 +0530

    Time: 634 ms (16.76%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,18 +1,15 @@
 # Write your MySQL query statement below
-with uniq_latlong as (
-    select lat,lon
-    from insurance
-    group by lat,lon
-    having count(*)=1
-),
-same_tiv15 as (
-    select tiv_2015
-    from insurance 
-    group by 1
-    having count(*)>1
+SELECT ROUND(SUM(tiv_2016), 2) AS TIV_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
 )
-
-select round(sum(a.tiv_2016),2) as tiv_2016
-from insurance a
-where (lat,lon) in (select lat,lon from uniq_latlong)
-and tiv_2015 in (select tiv_2015 from same_tiv15);
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
